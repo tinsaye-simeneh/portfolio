2c31f09ab78d7c7b379a9dddce67a86a8e3aaa87
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-19 02:22:25 +0300

    home page sidebar icon fixed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,8 +4,15 @@ import {AiFillHome as HomeIcon} from 'react-icons/ai';
 
 const Sidebar = () => {
     return (
-        <div className='container'>
-            <HomeIcon />
+        <div className='container-fluid bg-warning'>
+            <div className='row bg-primary'>
+                <div className='col-md-1 bg-dark'>
+                    <div className='col-md-10 col-12 text-center pt-5'>
+                    <HomeIcon className='text-white' size={30}/>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
     }
--- src/index.js
+++ src/index.js
@@ -3,23 +3,19 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Home from "./Home";
-import Navbar from './Navbar';
+import Home from "./pages/Home";
+import Sidebar from './components/Sidebar';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 
 function App() {
 
-    if (localStorage.getItem("LoginStatus") === null) {
-        localStorage.setItem("LoginStatus", false);
-    }
-
     return(
         <div className="container-fluid">
         <BrowserRouter>
-        <Navbar />
+        <Sidebar />
         <Routes>
-            <Route path="Home" element={<Home />} />
+            <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
       </div>
