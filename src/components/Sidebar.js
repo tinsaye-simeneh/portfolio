e671569ff47235318f3f812d40fa01a383d08c1f
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-19 02:04:00 +0300

    Sidebar initialized

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+        <h2>Sidebar</h2>
+        </div>
+    );
+    }
+
+export default Sidebar;
