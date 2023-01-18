3cd5b90a101fd32d183cc90b336df7d5ae157a49
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-19 02:25:37 +0300

    Project icon added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 import {AiFillHome as HomeIcon} from 'react-icons/ai';
+import {VscProject as ProjectIcon} from 'react-icons/vsc';
 
 const Sidebar = () => {
     return (
         <div className='container-fluid bg-warning'>
             <div className='row bg-primary'>
-                <div className='col-md-1 bg-dark'>
-                    <div className='col-md-10 col-12 text-center pt-5'>
+                <div className='col-md-1'>
+                    <div className="row mx-auto bg-dark">
+                    <div className='col-md-10 col-12 text-center mx-auto pt-5 mt-3'>
                     <HomeIcon className='text-white' size={30}/>
+                    </div>
+                    </div>
+                    <div className="row mx-auto bg-dark">
+                    <div className='col-md-10 col-12 text-center mx-auto pt-5 mt-4'>
+                    <ProjectIcon className='text-white' size={30}/>
+                    </div>
                     </div>
                 </div>
             </div>
