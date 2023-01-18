4eb41a6c093b933294228b672588593940764b3d
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-19 02:47:25 +0300

    alignment fixed for icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,42 +5,44 @@ import { VscProject as ProjectIcon } from "react-icons/vsc";
 import { BsBookmarks as ResumeIcon } from "react-icons/bs";
 import { BsCodeSquare as SkillIcon } from "react-icons/bs";
 import { BiMessageSquareDots as ContactIcon } from "react-icons/bi";
+import { CiLight as ThemeIcon } from "react-icons/ci";
 
 import "../styles/Sidebar.css";
 
 const Sidebar = () => {
   return (
-    <div className="d-flex justify-content-between bg-primary">
-      <div className="row bg-black">
-        <div className="col-md-1 bg-black">
+        <div className="col-md-1 bg-black ml-auto float-left bg-primary">
           <div className="row mx-auto bg-black">
-            <div className="col-md-10 col-12 text-center mx-auto pt-5 mt-3">
+            <div className="col-md-8 col-12 text-center mx-auto py-3 mt-4">
               <HomeIcon className="text-white" size={30} />
             </div>
           </div>
           <div className="row mx-auto bg-black">
-            <div className="col-md-10 col-12 text-center mx-auto pt-5 mt-4">
+            <div className="col-md-8 col-12 text-center mx-auto py-4 mt-3">
               <ProjectIcon className="text-white" size={30} />
             </div>
           </div>
           <div className="row mx-auto bg-black">
-            <div className="col-md-10 col-12 text-center mx-auto pt-5 mt-4">
+            <div className="col-md-8 col-12 text-center mx-auto py-4 mt-3">
               <ResumeIcon className="text-white" size={30} />
             </div>
           </div>
           <div className="row mx-auto bg-black">
-            <div className="col-md-10 col-12 text-center mx-auto pt-5 mt-4">
+            <div className="col-md-8 col-12 text-center mx-auto py-4 mt-3">
               <SkillIcon className="text-white" size={30} />
             </div>
           </div>
           <div className="row mx-auto bg-black">
-            <div className="col-md-10 col-12 text-center mx-auto pt-5 mt-4">
+            <div className="col-md-8 col-12 text-center mx-auto py-4 mt-4">
               <ContactIcon className="text-white" size={30} />
             </div>
           </div>
+          <div className="row mx-auto bg-black mt-5 pt-4">
+            <div className="col-md-10 col-12 text-center mx-auto mt-5 pb-5">
+              <ThemeIcon className="text-white" size={30} />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
   );
 };
 
