bb30f1c1f2a8ef145dc078ac67f0eee774c7d8c0
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-19 17:16:52 +0300

    Project card template file created

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import {BsGithub as GithubIcon} from "react-icons/bs";
+import {BsLink45Deg as LinkIcon} from "react-icons/bs";
+
+const ProjectCard = ({ title, image, GithubLink, DemoLink}) => {
+
+    if(DemoLink === undefined) DemoLink = "#";
+
+    return (
+        <div className="col-md-4 col-12 mx-auto">
+        <div className="card bg-dark rounded">
+            <img src={image} className="card-img-top" alt="..." />
+            <div className="card-body bg-white rounded">
+            <h5 className="card-title text-dark text-center">{title}</h5>
+            <hr className="text-dark" />
+            <div className="row mx-auto">
+                <div className="ms-auto col-md-6">
+                <a href={GithubLink} className="btn btn-secondary">
+                    <GithubIcon size={20} className="mb-1" />
+                    <span className="ms-2">Github</span>
+                </a>
+                </div>
+                <div className="ms-auto col-md-6">
+                <a href={DemoLink} className="btn btn-secondary">
+                    <LinkIcon size={20} />
+                    <span className="ms-1">Demo</span>
+                </a>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    );
+    }
+
+export default ProjectCard;
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,12 +2,11 @@ import React from "react";
 
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
+import ProjectCard from "../components/ProjectCard";
 
-import '../styles/Project.css'
+import "../styles/Project.css";
 
 import Coding from "../assets/Images/coding.jpg";
-import {BsGithub as GithubIcon} from "react-icons/bs";
-import {BsLink45Deg as LinkIcon} from "react-icons/bs";
 
 const Project = () => {
   return (
@@ -16,155 +15,16 @@ const Project = () => {
       <div className="container my-4 bg-dark me-auto me-5 ps-5">
         <Topbar PageName="Project" />
         <div className="border border-white mt-5">
-        <div className="row bg-dark py-5 mx-auto p-3 px-5">
-          <div className="col-md-4 col-12 mx-auto">
-            <div className="card bg-dark border-0">
-              <img src={Coding} className="card-img-top" alt="..." />
-              <div className="card-body bg-white rounded">
-                <h5 className="card-title text-dark text-center">Weather App</h5>
-                <hr className="text-dark"/>
-                <div className="row mx-auto">
-                <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <GithubIcon size={20} className="mb-1"/>
-                    <span className="ms-2">Github</span>
-                    </a>
-                  </div>
-                  <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <LinkIcon size={20}/>
-                    <span className="ms-1">Demo</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="row bg-dark py-5 mx-auto p-3 px-5">
+            <ProjectCard title="Weather App" image={Coding}/>
+            <ProjectCard />
+            <ProjectCard />
           </div>
-
-          <div className="col-md-4 mx-5 col-12 mx-auto">
-            <div className="card bg-dark rounded">
-              <img src={Coding} className="card-img-top" alt="..." />
-              <div className="card-body bg-white rounded">
-                <h5 className="card-title text-dark text-center">Weather App</h5>
-                <hr className="text-dark"/>
-                <div className="row mx-auto">
-                <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <GithubIcon size={20} className="mb-1"/>
-                    <span className="ms-2">Github</span>
-                    </a>
-                  </div>
-                  <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <LinkIcon size={20}/>
-                    <span className="ms-1">Demo</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 col-12 mx-auto">
-            <div className="card bg-dark rounded">
-              <img src={Coding} className="card-img-top" alt="..." />
-              <div className="card-body bg-white rounded">
-                <h5 className="card-title text-dark text-center">Weather App</h5>
-                <hr className="text-dark"/>
-                <div className="row mx-auto">
-                <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <GithubIcon size={20} className="mb-1"/>
-                    <span className="ms-2">Github</span>
-                    </a>
-                  </div>
-                  <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <LinkIcon size={20}/>
-                    <span className="ms-1">Demo</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
           <div className="row bg-dark pb-5 mx-auto p-3 px-5">
-          <div className="col-md-4 col-12 mx-auto">
-            <div className="card bg-dark rounded">
-              <img src={Coding} className="card-img-top" alt="..." />
-              <div className="card-body bg-white rounded">
-                <h5 className="card-title text-dark text-center">Weather App</h5>
-                <hr className="text-dark"/>
-                <div className="row mx-auto">
-                <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <GithubIcon size={20} className="mb-1"/>
-                    <span className="ms-2">Github</span>
-                    </a>
-                  </div>
-                  <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <LinkIcon size={20}/>
-                    <span className="ms-1">Demo</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
           </div>
-
-          <div className="col-md-4 col-12 mx-auto">
-            <div className="card bg-dark rounded">
-              <img src={Coding} className="card-img-top" alt="..." />
-              <div className="card-body bg-white rounded">
-                <h5 className="card-title text-dark text-center">Weather App</h5>
-                <hr className="text-dark"/>
-                <div className="row mx-auto">
-                <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <GithubIcon size={20} className="mb-1"/>
-                    <span className="ms-2">Github</span>
-                    </a>
-                  </div>
-                  <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <LinkIcon size={20}/>
-                    <span className="ms-1">Demo</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4 col-12 mx-auto">
-            <div className="card bg-dark rounded">
-              <img src={Coding} className="card-img-top" alt="..." />
-              <div className="card-body bg-white rounded">
-                <h5 className="card-title text-dark text-center">Weather App</h5>
-                <hr className="text-dark"/>
-                <div className="row mx-auto">
-                <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <GithubIcon size={20} className="mb-1"/>
-                    <span className="ms-2">Github</span>
-                    </a>
-                  </div>
-                  <div className="ms-auto col-md-6">
-                    <a href="#" className="btn btn-secondary">
-                    <LinkIcon size={20}/>
-                    <span className="ms-1">Demo</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
         </div>
       </div>
     </>
