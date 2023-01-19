d1b3f3e222ec04c1c88bd58570b5654dda1e1bdd
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-19 03:38:38 +0300

    Home page details final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,15 @@ const Home = () => {
         <div className="container bg-primary mt-4 bg-dark me-auto">
         <Topbar />
             <div className="row">
-                <div className="col-md-6 col-12">
-                    <h1 className="text-white">Hello, I'm <span className="text-primary">Sajjad</span></h1>
-                    <h1 className="text-white">I'm a <span className="text-primary">Front-End Developer</span></h1>
-                    <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                    <button className="btn btn-primary">Hire Me</button>
-                </div>
+               
                 <div className="col-md-6 col-12">
                     <img src={Image} alt="undraw-Developer-re" border="0" height={400}/>
+                </div>
+                <div className="col-md-6 col-12 mt-5">
+                    <h3 className="text-white">Hello, I'm <span className="text-primary">TINSAYE SIMENEH DESSIE</span></h3>
+                    <h1 className="text-white">I'm a <span className="text-primary">Front-End Developer</span></h1>
+                    <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                    <button className="btn btn-primary mt-3">Hire Me</button>
                 </div>
             </div>
         </div>
