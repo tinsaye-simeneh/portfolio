55d3201d739d4a3296570f742eaad8945cca8ae5
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-19 14:46:50 +0300

    Project file created

---
+++ src/pages/Project.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+
+const Project = () => {
+    return (
+        <>
+        <Sidebar ProjectIconColor="text-primary" />
+        <div className="container bg-primary mt-4 bg-dark me-auto">
+            <Topbar PageName="Project" />
+            <div className="row mt-5 pt-5">
+            <div className="col-md-6 col-12">
+                <h1 className="text-white">Project</h1>
+            </div>
+            </div>
+        </div>
+        </>
+    );
+    }
+
+export default Project;
