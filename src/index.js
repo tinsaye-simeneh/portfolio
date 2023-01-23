6b8bcf34482c86bdcf9a8ef163b9d16093132d2c
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-24 00:02:21 +0300

    bug fixed

--- src/index.js
+++ src/index.js
@@ -2,32 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap/dist/js/bootstrap.min.js";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
 import Resume from "./pages/Resume";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Skills from "./pages/Skills";
-import Contact from './pages/Contact'
-
+import Contact from "./pages/Contact";
 
 function App() {
-
-    return(
-        <div className="d-flex justify-content-between bg-dark">
-        <BrowserRouter>
+  return (
+    <div className="d-flex justify-content-between bg-dark">
+      <BrowserRouter>
         <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/skill" element={<Skills/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/" element={<Home/>}/>
-            <Route path="*" element={<Home/>}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/skill" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
-      </div>
-    );
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
