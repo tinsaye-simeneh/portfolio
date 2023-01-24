00c6d5686671395200b50b7becec7781a1e6d430
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-24 10:28:51 +0300

    navbar icon functionality fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
