73d67e7d9b40c97fcb1fa1d08bd06bd17ab4a68c
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2023-01-25 22:15:26 +0300

    Final bug fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,16 +8,17 @@ import { BsGithub as GithubIcon } from "react-icons/bs";
 import { ImTelegram as TelegramIcon } from "react-icons/im";
 import { AiFillLinkedin as LinkedinIcon } from "react-icons/ai";
 import { ImBehance2 as BehanceIcon } from "react-icons/im";
+import { MdEmail } from "react-icons/md";
 
 import Image from "../assets/Images/memoji.png";
 
-import '../styles/index.css'
+import "../styles/index.css";
 
 const Home = () => {
   return (
     <>
       <Sidebar HomeIconColor="text-primary" />
-      <div className="container main-container bg-dark pb-3">
+      <div className="container main-container bg-dark pb-1">
         <SidebarMobile HomeIconColor="text-primary" />
         <div className="row py-4 mx-auto pb-5 mb-4">
           <div className="col-md-6 col-12 mx-auto">
@@ -31,8 +32,7 @@ const Home = () => {
           </div>
           <div className="col-md-6 col-12 home-txt">
             <h3 className="text-white">
-              Hey, I'm{" "}
-              <span className="text-primary">TINSAYE SIMENEH</span>
+              Hey, I'm <span className="text-primary">TINSAYE SIMENEH</span>
             </h3>
             <h1 className="text-white">
               I'm a <span className="text-primary">Front-End Developer</span>
@@ -44,40 +44,50 @@ const Home = () => {
               technical feasibility. Implemented new responsive website Design
               and Development approach which increased mobile traffic.
             </p>
-            <div class="mx-auto mt-4 mb-5 text-white">
-                My Social Links:
-                <a
-                  href="https://github.com/tinsaye-simeneh"
-                  target="_blank"
-                  class="text-white ms-4 me-5"
-                >
-                  <GithubIcon size={30} />
-                </a>
-                <a
-                  href="https://t.me/TinsayeSimeneh"
-                  target="_blank"
-                  class="text-white me-5"
-                >
-                  <TelegramIcon size={30} />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/tinsayesimeneh"
-                  target="_blank"
-                  class="text-white me-5"
-                >
-                  <LinkedinIcon size={30} />
-                </a>
-                <a
-                  href="https://www.behance.net/tinsayesimeneh"
-                  target="_blank"
-                  class="text-white me-5"
-                >
-                  <BehanceIcon size={30} />
-                </a>
-              </div>
+            <div class="mx-auto my-5 text-white social-links">
+              <a
+                href="https://github.com/tinsaye-simeneh"
+                target="_blank"
+                class="text-white me-4"
+              >
+                <GithubIcon size={30} />
+              </a>
+              <a
+                href="https://t.me/TinsayeSimeneh"
+                target="_blank"
+                class="text-white me-4"
+              >
+                <TelegramIcon size={30} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/tinsayesimeneh"
+                target="_blank"
+                class="text-white me-4"
+              >
+                <LinkedinIcon size={30} />
+              </a>
+              <a
+                href="https://www.behance.net/tinsayesimeneh"
+                target="_blank"
+                class="text-white me-4"
+              >
+                <BehanceIcon size={30} />
+              </a>
+              <a
+                href="mailto:tinsayesimeneh608@gmail.com"
+                target="_blank"
+                class="text-white me-4"
+              >
+                <MdEmail size={30} />
+              </a>
+            </div>
             <div className="home-btn">
-            <a className="btn btn-dark border my-2 me-5" href="/project">See My Work</a>
-            <a className="btn btn-primary my-2" href="/contact">Hire Me</a>
+              <a className="btn btn-dark border my-2 me-5" href="/project">
+                See My Work
+              </a>
+              <a className="btn btn-primary my-2" href="/contact">
+                Hire Me
+              </a>
             </div>
           </div>
         </div>
