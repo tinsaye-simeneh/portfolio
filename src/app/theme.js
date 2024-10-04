537a308417dd12dccca5f5d6b81709e88b70280e
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-10-04 21:40:05 +0300

    feat: chackra setup

---
+++ src/app/theme.js
@@ -0,0 +1,16 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+      900: "#1a202c",
+    },
+  },
+  fonts: {
+    heading: "'Poppins', sans-serif",
+    body: "'Roboto', sans-serif",
+  },
+});
+
+export default theme;
