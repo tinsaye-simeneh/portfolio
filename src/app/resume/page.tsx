54a554a726e3b2aaef13a44da2048ac374a5120a
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-10-24 21:22:48 +0300

    fix: resume width and embed

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,24 +4,22 @@ import { Box, Heading } from "@chakra-ui/react";
 export default function ResumePage() {
   return (
     <Box
-      maxW="100%"
-      mx="auto"
-      mt={10}
-      px={4}
-      height="100vh"
+      w="95vw"
+      h="100vh"
       display="flex"
       flexDirection="column"
+      justifyContent="center"
       alignItems="center"
     >
-      <Heading as="h1" size="lg" mb={6}>
+      <Heading as="h1" size="lg" mb={6} textAlign="center">
         My Resume
       </Heading>
       <Box
         as="iframe"
         src="https://docs.google.com/document/d/e/2PACX-1vQMgPneXaqOhVcbDnfI-RkF6xMYQIrL6j4PeKbqFCPU1KvjO1xO9rbP4ZZ4N6yEw-VgeCQcMUFYNuPF/pub?embedded=true"
-        width="100%"
-        height="100%"
-        border="none"
+        width="70vw"
+        height="80vh"
+        border="2px solid"
       />
     </Box>
   );
