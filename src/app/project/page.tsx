80deb37f161089d5bd859fbbd2978d91ce10921c
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-10-26 23:02:52 +0300

    fix: card data moved to arrays

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,28 +1,36 @@
 import { SimpleGrid, Container } from "@chakra-ui/react";
 import ProjectCard from "../components/ProjectCard";
 
+const projects = [
+  {
+    title: "Project One",
+    description:
+      "This is a description of project one. It is a very interesting project.",
+    demoLink: "https://project-one-demo.com",
+    codeLink: "https://github.com/user/project-one",
+  },
+  {
+    title: "Project Two",
+    description:
+      "This is a description of project two. It is also a very interesting project.",
+    demoLink: "https://project-two-demo.com",
+    codeLink: "https://github.com/user/project-two",
+  },
+];
+
 export default function Projects() {
   return (
     <Container maxW="container.xl" py={10}>
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={8}>
-        <ProjectCard
-          title="Project One"
-          description="This is a description of project one. It is a very interesting project."
-          demoLink="https://project-one-demo.com"
-          codeLink="https://github.com/user/project-one"
-        />
-        <ProjectCard
-          title="Project Two"
-          description="This is a description of project two. It is also a very interesting project."
-          demoLink="https://project-two-demo.com"
-          codeLink="https://github.com/user/project-two"
-        />
-        <ProjectCard
-          title="Project Two"
-          description="This is a description of project two. It is also a very interesting project."
-          demoLink="https://project-two-demo.com"
-          codeLink="https://github.com/user/project-two"
-        />
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            description={project.description}
+            demoLink={project.demoLink}
+            codeLink={project.codeLink}
+          />
+        ))}
       </SimpleGrid>
     </Container>
   );
