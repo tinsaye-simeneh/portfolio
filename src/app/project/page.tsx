cb92d24715225409c377fe6a9b0ee9b56206c347
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-10-26 23:01:41 +0300

    fix: cards aligned by row

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,10 +1,10 @@
-import { VStack, Container } from "@chakra-ui/react";
+import { SimpleGrid, Container } from "@chakra-ui/react";
 import ProjectCard from "../components/ProjectCard";
 
 export default function Projects() {
   return (
-    <Container maxW="container.lg" py={10}>
-      <VStack spacing={8}>
+    <Container maxW="container.xl" py={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={8}>
         <ProjectCard
           title="Project One"
           description="This is a description of project one. It is a very interesting project."
@@ -17,7 +17,13 @@ export default function Projects() {
           demoLink="https://project-two-demo.com"
           codeLink="https://github.com/user/project-two"
         />
-      </VStack>
+        <ProjectCard
+          title="Project Two"
+          description="This is a description of project two. It is also a very interesting project."
+          demoLink="https://project-two-demo.com"
+          codeLink="https://github.com/user/project-two"
+        />
+      </SimpleGrid>
     </Container>
   );
 }
