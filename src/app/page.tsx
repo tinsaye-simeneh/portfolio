88e6867e7975794c9c33b1b16c2108d04877da91
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-10-27 01:19:43 +0300

    fix: hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 "use client";
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Button, Heading, Stack } from "@chakra-ui/react";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <Box>
+    <Box bg="gray.50" p={4}>
       <Hero />
+      <Container maxW="container.xl" my={10}>
+        <Stack spacing={8}>
+          <Heading as="h2" size="xl" textAlign="center">
+            Want to contact me?
+          </Heading>
+
+          <Button
+            colorScheme="teal"
+            size="lg"
+            alignSelf="center"
+            onClick={() => window.open("/contact-me", "_self")}
+          >
+            Contact Me
+          </Button>
+        </Stack>
+      </Container>
     </Box>
   );
 }
