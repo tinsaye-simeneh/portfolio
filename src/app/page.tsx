d953c9db125659c50967597255e2434d3068f2c2
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-10-27 20:04:18 +0300

    feat: home page project redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
 "use client";
-import { Container, Stack, Heading, Button, Box, Text } from "@chakra-ui/react";
-
+import {
+  Container,
+  Stack,
+  Heading,
+  Button,
+  Box,
+  Text,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import ProjectCard from "./components/ProjectCard";
 import Hero from "./components/Hero";
+import Link from "next/link";
 
 export default function Home() {
+  const projects = [
+    {
+      title: "Jiret",
+      demoLink: "https://jiret.com",
+      description: "A comprehensive client management system.",
+      codeLink: "https://github.comtinsaye-simeneh/jiret",
+      languages: ["JavaScript", "React"],
+    },
+    {
+      title: "Muyalogy",
+      demoLink: "https://muyalogy.com",
+      description: "An online learning management system.",
+      codeLink: "https://github.comtinsaye-simeneh/muyalogy",
+      languages: ["JavaScript", "React"],
+    },
+    {
+      title: "Tin ET",
+      demoLink: "https://tin-et.vercel.app/",
+      description: "An engaging e-commerce platform.",
+      codeLink: "https://github.comtinsaye-simeneh/tin-et",
+      languages: ["JavaScript", "Next.js"],
+    },
+  ];
+
   return (
     <Box p={4}>
       <Hero />
@@ -26,9 +59,29 @@ export default function Home() {
             Contact Me
           </Button>
 
-          <Heading as="h3" size="lg" textAlign="center" mt={20}>
-            Featured Projects
-          </Heading>
+          <Stack spacing={8} py={10} mx={"auto"} textAlign={"center"}>
+            <Heading as="h3" size="lg" textAlign="center" mt={20}>
+              Featured Projects
+            </Heading>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={8}>
+              {projects.map((project, index) => (
+                <ProjectCard
+                  key={index}
+                  title={project.title}
+                  description={project.description}
+                  demoLink={project.demoLink}
+                  codeLink={project.codeLink}
+                  languages={project.languages}
+                />
+              ))}
+            </SimpleGrid>
+
+            <Link href="/projects">
+              <Button alignSelf="center" colorScheme="teal" size="lg">
+                View All Projects
+              </Button>
+            </Link>
+          </Stack>
         </Stack>
       </Container>
     </Box>
